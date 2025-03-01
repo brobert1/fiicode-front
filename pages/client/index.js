834aa6da86e906854d208e41b337a64f89f68dab
Bercaru Robert <robert.bercaru@chesscoders.com>
2025-03-01 18:09:40 +0200

    component refactoring

--- pages/client/index.js
+++ pages/client/index.js
@@ -1,14 +1,17 @@
 import { checkAuth, withAuth } from "@auth";
 import { ClientLayout } from "@components";
 import { GoogleMap } from "@components/GoogleMaps";
+import { useUserLocation } from "@hooks";
 import MapSearchProvider from "contexts/MapSearchContext";
 
 const Page = () => {
+  const { location, loading, error, refreshLocation } = useUserLocation();
+
   return (
     <MapSearchProvider>
       <ClientLayout>
         <div className="h-full w-full">
-          <GoogleMap />
+          <GoogleMap {...{ location, loading, error, refreshLocation }} />
         </div>
       </ClientLayout>
     </MapSearchProvider>

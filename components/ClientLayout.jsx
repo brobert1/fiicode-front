2b9b82d0ffa04ce827ca497148c0f4f0bba93f27
Bercaru Robert <robert.bercaru@chesscoders.com>
2025-03-01 18:02:46 +0200

    refactor client layou

--- components/ClientLayout.jsx
+++ components/ClientLayout.jsx
@@ -1,16 +1,13 @@
 import { FloatingMenu } from "@components";
-import MapSearchProvider from "contexts/MapSearchContext";
 
 const ClientLayout = ({ children }) => {
   return (
-    <MapSearchProvider>
-      <div className="flex min-h-screen font-body text-sm">
-        <main className="h-screen w-full relative">
-          <div className="h-full w-full">{children}</div>
-          <FloatingMenu />
-        </main>
-      </div>
-    </MapSearchProvider>
+    <div className="flex min-h-screen font-body text-sm">
+      <main className="h-screen w-full relative">
+        <div className="h-full w-full">{children}</div>
+        <FloatingMenu />
+      </main>
+    </div>
   );
 };
 
--- pages/client/index.js
+++ pages/client/index.js
@@ -1,14 +1,17 @@
 import { checkAuth, withAuth } from "@auth";
 import { ClientLayout } from "@components";
 import { GoogleMap } from "@components/GoogleMaps";
+import MapSearchProvider from "contexts/MapSearchContext";
 
 const Page = () => {
   return (
-    <ClientLayout>
-      <div className="h-full w-full">
-        <GoogleMap />
-      </div>
-    </ClientLayout>
+    <MapSearchProvider>
+      <ClientLayout>
+        <div className="h-full w-full">
+          <GoogleMap />
+        </div>
+      </ClientLayout>
+    </MapSearchProvider>
   );
 };
 

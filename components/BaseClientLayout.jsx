6cdc2304adfca8be356af5c33e3c834756b0b58a
Bercaru Robert <robert.bercaru@chesscoders.com>
2025-03-02 22:42:03 +0200

    modified layout

--- components/BaseClientLayout.jsx
+++ components/BaseClientLayout.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const BaseClientLayout = ({ children, floatingMenu = null }) => {
   return (
     <div className="flex min-h-screen font-body text-sm">
-      <main className="h-screen w-full relative">
-        <div className="h-full w-full">{children}</div>
+      <main className="min-h-screen w-full relative">
+        <div className="h-screen w-full">{children}</div>
         {floatingMenu}
       </main>
     </div>
--- components/MapLayoutPages.jsx
+++ components/MapLayoutPages.jsx
@@ -1,6 +1,8 @@
 import { Button, MenuItem } from "@components";
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { MapSearchContext } from "contexts/MapSearchContext";
+import { useDisclosure, useOnClickOutside } from "@hooks";
+import { logout } from "@api/identity";
 
 const MapLayoutPages = () => {
   const mapSearchContext = useContext(MapSearchContext);
@@ -9,7 +11,16 @@ const MapLayoutPages = () => {
     setSearchVisible: () => {},
   };
 
+  const { isOpen, toggle, hide } = useDisclosure();
+  const menuRef = useRef();
+  useOnClickOutside(menuRef, hide);
+
   const isMapPage = !!mapSearchContext;
+
+  const handleLogout = async () => {
+    hide();
+    await logout();
+  };
 
   return (
     <>
@@ -36,12 +47,28 @@ const MapLayoutPages = () => {
       >
         <i className="fa fa-bell"></i>
       </MenuItem>
-      <MenuItem
-        href="#"
-        className="group text-lg flex h-10 items-center justify-center rounded-full px-3 transition-colors md:px-4"
-      >
-        <i className="fa fa-gear"></i>
-      </MenuItem>
+      <div className="relative inline-block">
+        <Button
+          className="group text-lg flex h-10 items-center justify-center rounded-full px-3 transition-colors md:px-4"
+          onClick={toggle}
+          ref={menuRef}
+        >
+          <i
+            className={`fa fa-gear transition-transform duration-300 ${isOpen ? "rotate-90" : ""}`}
+          ></i>
+        </Button>
+        {isOpen && (
+          <div className="absolute left-1/2 bottom-full mb-2 w-48 bg-white  rounded-md shadow-lg py-1 z-20 transform -translate-x-1/2 transition-opacity duration-200 ease-in-out opacity-100">
+            <button
+              className="flex w-full items-center px-4 py-2 text-sm text-red-500 hover:bg-gray-100"
+              onClick={handleLogout}
+            >
+              <i className="fas fa-sign-out-alt w-6"></i>
+              Logout
+            </button>
+          </div>
+        )}
+      </div>
       <MenuItem
         href="/client/account"
         className="group text-lg flex h-10 items-center justify-center rounded-full px-3 transition-colors md:px-4"
